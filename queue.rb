86ea2a351762adfaa7701b2b23e0b9538661998b
Greg Fisher <me@gnfisher.com>
2018-10-16 15:22:08 -0300

    toying around

---
+++ queue.rb
@@ -0,0 +1,27 @@
+# Playing around with select, IO, listening for key press etc..
+require "io/console"
+require "pry"
+
+# this messes up new lines etc how to fix quickly?
+system("stty raw -echo")
+loop do
+  read, _, _ = select([STDIN], nil, nil)
+  if read.include?(STDIN)
+    if STDIN.getc == ?q
+      raise SystemExit
+    else
+      puts "No"
+    end
+  end
+end
+
+# loop do
+#   # getch is blocking! waits for input!
+#   # x = STDIN.getch
+#   case x
+#   when ?j then puts "HELLO"
+#   when ?k then puts "GOODBYE"
+#   when ?\C-c then raise SystemExit
+#   end
+# end
+
